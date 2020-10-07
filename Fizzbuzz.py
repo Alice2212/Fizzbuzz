88a7d47b1bd380e6dbc5e4b7d2fe4d2e6737cce0
Oluwatosin <Hezealice@gmail.com>
2020-10-07 14:27:22 +0100

    adding a file to the nwebaranch

--- Fizzbuzz.py
+++ Fizzbuzz.py
@@ -6,10 +6,10 @@ def fizzbuzz(n):
 		add = ''
 		# check if there is a remainder when dividing by 3, if not
 		# then we know this number is divisible by 3
-		if i % 3 == 0:
+		if i % 5 == 0:
 			add += 'Fizz'
 		# check if divisible by 5
-		if i % 5 == 0:
+		if i % 9 == 0:
 			add += 'Buzz'
 			# not divisible by either 3 or 5
 		if add == '':
@@ -17,4 +17,4 @@ def fizzbuzz(n):
 		else:
 			result.append(add)
 	print(result)
-fizzbuzz(101)
+fizzbuzz(100)
